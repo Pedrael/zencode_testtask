d5fb40667fdc494a57d1103e853175cb04d94794
Pedrael <45235838+Pedrael@users.noreply.github.com>
2023-07-05 16:22:46 +0300

    feat: First iteration of create markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Navmenu } from './components/Navmenu'
+import { usePage } from 'vite-plugin-ssr'
+
 const App = () => {
   return (
     <>
       <h1>Test</h1>
+      <Navmenu />
     </>
   )
 }
---
+++ src/components/Navmenu.tsx
@@ -0,0 +1,9 @@
+import { Avatar, Stack } from '@mui/material'
+
+export const Navmenu = () => {
+  return (
+    <Stack alignItems="center">
+      <Avatar></Avatar>
+    </Stack>
+  )
+}
