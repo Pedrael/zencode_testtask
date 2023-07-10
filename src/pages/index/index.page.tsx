2d729ab8cdf26343126531b248d7d26b0b482046
Pedrael <45235838+Pedrael@users.noreply.github.com>
2023-07-10 19:02:47 +0300

    fix: Move Orders list to index

--- src/pages/index/index.page.tsx
+++ src/pages/index/index.page.tsx
@@ -1,7 +1,23 @@
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store'
+import { OrderCard } from '../../components/orderCard'
+
 export const Page = () => {
+  const { id, title, date, description, products } = useSelector(
+    (state: RootState) => state.order.value[0],
+  )
   return (
     <>
-      <h1>Index</h1>
+      <h1>Products</h1>
+      <div>
+        <OrderCard
+          id={id}
+          title={title}
+          date={date}
+          description={description}
+          products={[...products]}
+        />
+      </div>
     </>
   )
 }
